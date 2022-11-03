1d05422121516a955da73d2fd78be85db579a2f7
Rikatob <rikatob@hotmail.com>
2022-11-03 23:51:54 +0100

    Player class started. * Created  * constructor  * GetNextUnit()--> Not good, but good enough for now  * AttackedBy() --> sends the value down to army method of same name.

---
+++ SoftwareDesignEksamen/player/Player.cs
@@ -0,0 +1,31 @@
+﻿using SoftwareDesignEksamen.army;
+using SoftwareDesignEksamen.unit;
+
+namespace SoftwareDesignEksamen.player;
+
+public class Player
+{
+    private Army _army;
+
+    public Player(Army army, string name, int gold)
+    {
+        _army = army;
+        Name = name;
+        Gold = gold;
+    }
+
+    public string Name { get; set; }
+    public int Gold { get; set; }
+
+    // todo
+    // maybe change this later on to not only give the first Unit but instead the next unit based by turn.
+    public AbstractUnit GetNextUnit()
+    {
+        return _army.Units[0];
+    }
+
+    public void AttackedBy(AbstractUnit attacker)
+    {
+        _army.AttackedBy(attacker);
+    }
+}
