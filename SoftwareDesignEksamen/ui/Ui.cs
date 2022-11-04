65451f6404bde8f96d62c07f08974040f878738f
Rikatob <89411878+Rikatob@users.noreply.github.com>
2022-11-04 17:01:09 +0100

    -UI- * added stub methods to Ui class.

--- SoftwareDesignEksamen/ui/Ui.cs
+++ SoftwareDesignEksamen/ui/Ui.cs
@@ -2,9 +2,37 @@ namespace SoftwareDesignEksamen.ui;
 
 public class Ui
 {
+    public int FontSize { get; set; }
+    public ConsoleColor Color { get; set; }
+
+    #region Methods
+
+    public void PrintGameBoard()
+    {
+    }
+
+    public void PrintStartMenu()
+    {
+    }
+
+    public int PrintultipleChoice()
+    {
+        return -1;
+    }
+
+    public int ReadInt()
+    {
+        return -1;
+    }
+
     public string ReadString()
     {
         return Console.ReadLine();
+    }
+
+    public bool ReadBoolean()
+    {
+        return false;
     }
 
     public string AskQuestion(string question)
@@ -14,8 +42,11 @@ public class Ui
     }
 
     // Print message to console
+
     public void Message(string message)
     {
         Console.WriteLine(message);
     }
+
+    #endregion
 }
