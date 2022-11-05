c7d82a7afe7602489799915ebd5c2f6d4e98928c
Rikatob <rikatob@hotmail.com>
2022-11-05 14:52:29 +0100

    UnitFactory * Created basic factory methods for creating a unit and upgrading a unit. * Created UnitEnum and DecoratorEnum for easy use in factory Mehtods.

---
+++ SoftwareDesignEksamen/unit/unitDecorator/DecoratorEnum.cs
@@ -0,0 +1,7 @@
+﻿namespace SoftwareDesignEksamen.unit.unitDecorator;
+
+public enum DecoratorEnum
+{
+    SWORD = 0,
+    SHIELD = 1,
+}
---
+++ SoftwareDesignEksamen/unit/unitFactory/UnitFactory.cs
@@ -0,0 +1,27 @@
+﻿using SoftwareDesignEksamen.unit.unitDecorator;
+
+namespace SoftwareDesignEksamen.unit.unitFactory;
+
+public class UnitFactory
+{
+    public AbstractUnit? CreateUnit(UnitEnum unit)
+    {
+        switch (unit)
+        {
+            case UnitEnum.DPS: return new DpsUnit();
+            case UnitEnum.TANK: return new TankUnit();
+            case UnitEnum.HEALER: return new HealerUnit();
+            default: return null;
+        }
+    }
+
+    public AbstractUnit? UpgradeUnit(AbstractUnit unit, DecoratorEnum upgrade)
+    {
+        switch (upgrade)
+        {
+            case DecoratorEnum.SWORD: return new SwordDecorator(unit);
+            case DecoratorEnum.SHIELD: return new ShieldDecorator(unit);
+            default: return unit;
+        }
+    }
+}
