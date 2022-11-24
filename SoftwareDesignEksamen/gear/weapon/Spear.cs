fc82b42b82fae9ff7c6a587abd69e04c5020067c
Fargekritt <Lullinj98@gmail.com>
2022-11-24 15:48:34 +0100

    Spear now has values

--- SoftwareDesignEksamen/gear/weapon/Spear.cs
+++ SoftwareDesignEksamen/gear/weapon/Spear.cs
@@ -4,4 +4,10 @@ namespace SoftwareDesignEksamen.unit.unitFactory;
 
 public class Spear : AbstractWeapon
 {
+    public Spear()
+    {
+        Cost = 2;
+        Damage = 30;
+        Reach = 1;
+    }
 }
