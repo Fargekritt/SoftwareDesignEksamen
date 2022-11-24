a599c94c96dfd608e90563b55c1cae9bf3acb220
Rikatob <rikatob@hotmail.com>
2022-11-24 15:18:27 +0100

    Testing * Created new NunitTest project * set up plan for testing.

---
+++ NunitTests/UnitTest1.cs
@@ -0,0 +1,45 @@
+namespace NunitTests;
+
+public class Tests
+{
+    
+    
+    //WEAPON
+    // Test weapons have correct values.
+    
+    // test adding more decorators have correct values (reach)
+    
+    // Test shield 
+    
+    // Test chestplate
+    
+    //UNIT
+    // Test unit elf human ...
+    
+    // Test unit decorators dps tank ...
+    
+    // test unit factory construct the right unit.is values correct. elf dps with weapon .
+    
+    
+    // ARMY
+    
+    // TEST methodes attackedBy, healingTurn .....
+    
+    
+    
+    
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+    
+    
+    
+}
